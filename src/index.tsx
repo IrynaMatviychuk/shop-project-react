8c352e95b6e37c72cca0b1e526dc3abee6aaaf80
Iryna Borshchyk <ira011019997@gmail.com>
2023-03-27 15:34:19 +0300

    add React Component; also add React.Fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// створюємо React Element - найпростіша структура в React
+// створення компоненту
+function Title() {
+    return <h1>Hello TS</h1>
+}
 
-// перший варіант запису (для розуміння роботи "під капотом") without jsx
-// const title = React.createElement('h1', { id: 'title' }, 'Hello, React.js')
-// console.log(title)
-// root.render(<React.StrictMode>{title}</React.StrictMode>)
+// ще один компонент, замість огортаючого div пишемо
+function List() {
+    return (
+        <React.Fragment>
+            {/* <div> */}
+            <ul>
+                <li>List item 1</li>
+                <li>List item 2</li>
+                <li>List item 3</li>
+            </ul>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptatibus quas quaerat corporis, modi amet ipsum eius cum
+                ducimus reiciendis necessitatibus voluptates eaque quia quos
+                repellendus reprehenderit excepturi, ex eum minus.
+            </p>
+            {/* </div> */}
+        </React.Fragment>
+    )
+}
 
-// другий варіант (простіший) за допомогою tsx extended,  вивдимо в <React.StrictMode>
-const title = <h1 id="title">Hello React.js</h1>
-console.log(title)
-
-// правила роботи з JSX
-const content = (
-    <div>
-        {title}
-        <ul className="list">
-            <li>Lost item 1</li>
-            <li>Lost item 2</li>
-            <li>Lost item 3</li>
-        </ul>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
-            natus eos enim, suscipit maiores ex laborum culpa doloremque
-            accusamus hic? Vel perferendis suscipit fuga dolor nesciunt
-            assumenda sapiente fugiat distinctio!
-        </p>
-    </div>
-)
+// створення кореневого компоненту, в який рендиримо два попередніх компоненти
+const App = () => {
+    return (
+        <div className="app">
+            <Title />
+            <List />
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{content}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        {/* <Title />
+        <List /> */}
+        <App />
+    </React.StrictMode>
+)
