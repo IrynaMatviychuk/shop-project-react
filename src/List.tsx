75ba55c85970fcbf65a176a3fa25af25c11f4ffc
Iryna Borshchyk <ira011019997@gmail.com>
2023-03-30 20:10:15 +0300

    Стилізація через створення файлу css та імпорт

--- src/List.tsx
+++ src/List.tsx
@@ -1,14 +1,9 @@
+import './List.css'
+
 type Props = {}
 
 // Іменована форма експорту
 export const List = (props: Props) => {
-    let data = 'red'
-    let data2 = true
-    const styleObj = {
-        color: 'white',
-        backgroundColor: 'blue',
-        padding: '10px',
-    }
     return (
         <>
             <ul>
@@ -16,19 +11,13 @@ export const List = (props: Props) => {
                 <li>List 2</li>
                 <li>List 3</li>
             </ul>
-            <p
-                style={{
-                    color: data2 ? 'blue' : 'green',
-                    backgroundColor: data,
-                    padding: '15px',
-                }}
-            >
+            <p className="content">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
                 exercitationem consequatur nulla. Autem, unde ad velit
                 cupiditate eius adipisci eum rerum iure quis maxime ducimus
                 aperiam, deserunt natus voluptate ab.
             </p>
-            <p style={styleObj}>
+            <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
                 exercitationem consequatur nulla. Autem, unde ad velit
                 cupiditate eius adipisci eum rerum iure quis maxime ducimus
