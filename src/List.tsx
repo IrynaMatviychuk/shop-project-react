2233a9e5690e540ac6c77b5cbca8bb442c21fd3b
Iryna Borshchyk <ira011019997@gmail.com>
2023-03-31 00:03:27 +0300

    Створили модулі для css

--- src/List.tsx
+++ src/List.tsx
@@ -1,10 +1,10 @@
-import './List.css'
+import classes from './List.module.css'
 
 type Props = {}
 
 // Іменована форма експорту
 export const List = (props: Props) => {
-    let data = true
+    let data = false
     return (
         <>
             <ul>
@@ -12,7 +12,11 @@ export const List = (props: Props) => {
                 <li>List 2</li>
                 <li>List 3</li>
             </ul>
-            <p className={`article-description ${data ? 'content' : 'test'}`}>
+            <p
+                className={`${classes['article-description']} ${
+                    data ? classes.content : classes.test
+                }`}
+            >
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
                 exercitationem consequatur nulla. Autem, unde ad velit
                 cupiditate eius adipisci eum rerum iure quis maxime ducimus
